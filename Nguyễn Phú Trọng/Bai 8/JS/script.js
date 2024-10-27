3776c232c8b81bffb9b008e771af03f0dce9ff09
gnort-nguyen <itzcybertactical@gmail.com>
2024-10-27 11:07:52 +0700

    Update script.js

--- Nguyễn Phú Trọng/Bai 8/JS/script.js
+++ Nguyễn Phú Trọng/Bai 8/JS/script.js
@@ -1,6 +1,12 @@
 
-document.getElementbyId("login").addeventListener('click',function(e) {
+document.getElementById("login").addEventListener('click',function(e) {
+
+    let usernamdata ={}
+
     let username = document.getElementById('username').value;
     let password = document.getElementById('password').value;
     console.log(username, password);
-})
+    if (username == 'admin' && password == '12345') {
+        console.log('admin')
+    }
+});
